47e7b2aca9c543d9281d2f697eb1132a439100e2
Artchibald <archie@archibaldbutler.com>
2023-07-04 16:37:43 +0100

    initial

---
+++ three_js/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="embed-wrapper"> <iframe title="Chinese Old Place" allowFullScreen allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/66e4ced42d3d4a9f8598241de7491783/embed"> </iframe></div>
+    </div>
+  );
+}
+
+export default App;
